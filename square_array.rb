630d9162eaf8b884e85aeddfb681cc2781348fd7
Andy Maskiell <maskiella@gmail.com>
2017-11-30 21:59:48 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def square_array(array)
   # your code here
+  squares = []
+  array.each do |num|
+    # binding.pry
+    squares << num**2
+  end
+  squares
+  # binding.pry
 end
